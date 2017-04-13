ce3f8f0210a8cd28c106dec39b1040f9d929f1b1
Atif <amnoori@users.noreply.github.com>
2017-04-12 18:15:19 -0700

    Added Error Modal

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div className="text-center">
-      <h1>About This Application</h1>
+      <h1>About</h1>
       <p>This is a test application created in React</p>
       <p>Weather data is courtesy of <a href='http://openweathermap.org' target="_blank">Open Weather Map</a></p>
     </div>
