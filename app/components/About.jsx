49a3f1ef3cd05a8303f71d9d4667599671c20478
Atif <amnoori@users.noreply.github.com>
2017-04-13 11:40:11 -0700

    Custom CSS

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,9 +3,9 @@ var React = require('react');
 var About = (props) => {
   return (
     <div className="text-center">
-      <h1>About</h1>
-      <p>This is a test application created in React</p>
-      <p>Weather data is courtesy of <a href='http://openweathermap.org' target="_blank">Open Weather Map</a></p>
+      <h1 className="page-title">About</h1>
+      <p>This is a test application created in React.</p>
+      <p>Weather data is courtesy of <a href='http://openweathermap.org' target="_blank">Open Weather Map</a>.</p>
     </div>
   )
 };
